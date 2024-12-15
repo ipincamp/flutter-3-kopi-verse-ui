9088bead9ea305cb86266eaf75cd3c81f2a39ee6
ipincamp <support@nur-arifin.my.id>
2024-12-15 09:59:17 +0700

    Scaffold cashier and customer screen

---
+++ lib/screen/cashier/transaction_history.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistory extends StatefulWidget {
+  const TransactionHistory({super.key});
+
+  @override
+  State<TransactionHistory> createState() => _TransactionHistoryState();
+}
+
+class _TransactionHistoryState extends State<TransactionHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Transaction History'),
+      ),
+    );
+  }
+}
---
+++ lib/screen/customer/catalog.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CatalogScreen extends StatefulWidget {
+  const CatalogScreen({super.key});
+
+  @override
+  State<CatalogScreen> createState() => _CatalogScreenState();
+}
+
+class _CatalogScreenState extends State<CatalogScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Catalog'),
+      ),
+    );
+  }
+}
